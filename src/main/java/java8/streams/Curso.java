98811929a8aa93ca2b59e96b33d83d93ae139276
nogard <silvanopessoa@gmail.com>
2017-08-21 22:28:22 -0300

    Signed-off-by: nogard <silvanopessoa@gmail.com>

--- src/main/java/java8/streams/Curso.java
+++ src/main/java/java8/streams/Curso.java
@@ -5,6 +5,7 @@ import static java.util.Comparator.comparingInt;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Curso {
@@ -30,6 +31,10 @@ public class Curso {
     	cursos.add(new Curso("JavaScript", 150));
     	cursos.add(new Curso("Java 8", 113));
     	cursos.add(new Curso("C", 55));
+    	
+    	Stream<Curso> stream = cursos.stream()
+    			   .filter(c -> c.getAlunos() > 50);
+    	stream.collect(Collectors.toList());
     	
     	double media = cursos.stream().mapToInt(c -> c.getAlunos()).average().getAsDouble();
     	System.out.println(media);
